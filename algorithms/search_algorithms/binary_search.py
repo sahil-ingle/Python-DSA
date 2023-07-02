5dffbe141abdfbb78193f06244a354fad2aeaaed
Sahil Ingle <inglesahil98@gmail.com>
2023-07-03 05:18:32 +0530

    updated algorithms

--- algorithms/search_algorithms/binary_search.py
+++ algorithms/search_algorithms/binary_search.py
@@ -1,21 +1,21 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
+def binary_search(arr, item):
 
-    while left <= right:
-        mid = (left + right) // 2
+    left_index = 0
+    right_index = len(arr) - 1
 
-        if arr[mid] == target:
+    while left_index <= right_index:
+        mid = (left_index + right_index) // 2
+
+        if arr[mid] == item:
             return mid
-
-        elif arr[mid] < target:
-            left = mid + 1
-
+        if item > arr[mid]:
+            left_index = mid + 1
         else:
-            right = mid - 1
+            right_index = mid - 1
 
     return -1
 
+
 x = [1,2,3,4,5,6]
 
-print(binary_search(x, 3))
+print(binary_search(x, 6))
