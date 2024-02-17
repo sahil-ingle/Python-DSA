b03b06d0bc02bb4d1a3dc62076ffdf3bbd410365
sahil-ingle <inglesahil00@gmail.com>
2024-02-17 20:27:45 +0530

    s1

--- test.py
+++ test.py
@@ -1,41 +1,46 @@
-class node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
+num = [2, 234, 234, 234, 234, 234, 423, 23, 235, 2535, 4, 5462, 6123, 63, 461, 345, 134, 635, 6, 256, 34]
 
-class link_list:
-    def __init__(self):
-        self.head = None
-    def is_empty(self):
-        return not self.head
+def merge_sort(arr):
+    if len(arr) <=1:
+        return arr
 
-    def append(self,data):
-        new_node = node(data)
-        if self.is_empty():
-            self.head = new_node
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j +1] = arr[j+1] , arr[j]
+
+    return arr
+
+
+print(merge_sort(num))
+
+
+def binary_serach(x, arr):
+    left = 0
+    right = len(arr) - 1
+    mid = (left + right)//2
+
+    for i in range(len(arr) - 1):
+        mid = (left + right)//2
+        if arr[mid] == x:
+            return "found it at index " + str(mid)
+        if arr[mid] < x:
+            left = mid
         else:
-            current_node = self.head
-            while current_node.next:
-                current_node = current_node.next
-            current_node.next = new_node
+            right = mid
 
-    def printa(self):
-        if self.is_empty():
-            return -1
-        else:
-            current_node = self.head
-            while current_node:
-                print(current_node.data)
-                current_node = current_node.next
+    return "its not in the list"
+
+print(binary_serach(234,num))
+
+n = "00000010100101000001111010011100"
+bit_num = int(n, 2)
+print(bit_num)
+
+bit_val = bin(bit_num)
+bit_val = str(bit_val)[2:]
+bit_val = bit_val[::-1]
+bit_num = int(bit_val, 2)
+print(bit_num)
 
 
-obj = link_list()
-
-obj.append(1)
-obj.append(2)
-obj.append(1)
-obj.append(2)
-obj.append(1)
-obj.append(2)
-
-obj.printa()
