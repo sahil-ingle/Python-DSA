8944b589a95e25ed025d2bdbf054be2b6c1dc72b
sahil-ingle <inglesahil00@gmail.com>
2024-02-18 15:46:20 +0530

    updated sorting algorithms

--- Sorting Algorithms/bubble_sort.py
+++ Sorting Algorithms/bubble_sort.py
@@ -3,11 +3,16 @@ def bubble_sort(arr):
         return arr
 
     for i in range(len(arr) - 1):
+        swap = False
         for j in range(len(arr) - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                swap = True
+        if not swap:
+            return arr
 
-    return arr
+
+
 
 x = [2,3 ,2,334,23,423,34,234,34 ,138]
 
